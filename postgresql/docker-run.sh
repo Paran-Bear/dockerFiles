f8caa28a85bf79a749ca11722f187c34dcee0bd9
zibby <znrdlsxjspt1@gmail.com>
2023-02-21 10:35:55 +0900

    add postgresql docker run shell

--- postgresql/docker-run.sh
+++ postgresql/docker-run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -p 5432:5432 --name postgres-ko-5432 -e POSTGRES_PASSWORD=1234 -e TZ=Asia/Seoul -v /postgreSQL-ko:/var/lib/postgresql/data -d --restart unless-stopped postgres-ko:latest
+docker run -p 5432:5432 --name postgres-ko-5432 -e POSTGRES_PASSWORD=1234 -e TZ=Asia/Seoul -v aibot30:/var/lib/postgresql/data -d --restart unless-stopped postgres-ko:latest
